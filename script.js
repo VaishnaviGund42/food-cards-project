bf4b3fbb489e326740267d2b2ffd46a3556f1162
VaishnaviGund42 <vaishnavigund42@gmail.com>
2025-09-06 23:12:00 +0530

    array added in the js file

---
+++ script.js
@@ -0,0 +1,8 @@
+// Array of favourite food items
+let foods = [
+  { name: "Pizza", image: "https://source.unsplash.com/200x150/?pizza" },
+  { name: "Burger", image: "https://source.unsplash.com/200x150/?burger" },
+  { name: "Pasta", image: "https://source.unsplash.com/200x150/?pasta" },
+  { name: "Ice Cream", image: "https://source.unsplash.com/200x150/?icecream" },
+  { name: "Sushi", image: "https://source.unsplash.com/200x150/?sushi" }
+];
