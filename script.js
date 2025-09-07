e5a14dc75212bf72a22b50892832eaa12b3a1f83
VaishnaviGund42 <vaishnavigund42@gmail.com>
2025-09-07 11:49:09 +0530

    update the food cards images

--- script.js
+++ script.js
@@ -3,8 +3,8 @@ let foods = [
   { name: "Pizza", image: "images/pizza-img.jpg" },
   { name: "Burger", image: "images/burger.jpg" },
   { name: "Pasta", image: "images/pasta.jpg" },
-  { name: "Ice Cream", image: "images/ice-cream.jpg" },
-  { name: "Sandwich", image: "images/sandwich.jpg" }
+  { name: "Ice Cream", image: "images/icecream.jpg" },
+  { name: "Sandwich", image: "images/sandwich-img.jpg" }
 ];
 // Get container
 let container = document.getElementById("food-container");
